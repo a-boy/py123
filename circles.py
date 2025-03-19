30ae5b231a5bd6fb575fb436bfb80c7b11ce7e25
a boy <a-boy@users.noreply.github.com>
2025-03-19 09:27:26 +0800

    Add files via upload

---
+++ circles.py
@@ -0,0 +1,4 @@
+from turtle import *
+color("lightgreen")
+for i in range(1,5):   
+    circle(i*20,360)
---
+++ rose.py
@@ -0,0 +1,6 @@
+import turtle as t 
+t.color('red')
+for i in range(270):
+    t.forward(i)
+    t.left(70)
+t.done()
